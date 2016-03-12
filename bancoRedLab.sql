41448920712cd956693289a075dd32093cad0dc4
Matheus Assis <adowtatep@hotmail.com.br>
2016-03-12 12:33:20 -0300

    Criado SQL para Database
    Algumas alterações no layout também foram feitas

--- bancoRedLab.sql
+++ bancoRedLab.sql
@@ -1,2 +1,45 @@
+DROP DATABASE IF EXISTS RedLab;
 CREATE DATABASE RedLab;
+USE RedLab;
 
+CREATE TABLE config(
+	id int primary key auto_increment,
+	custoExamePadrao double not null    
+);
+
+CREATE TABLE usuario(
+	login varchar(20) primary key,
+    senha varchar(50) not null,
+    isAdmin bool    
+);
+
+CREATE TABLE pessoa(	
+    login varchar(20) primary key,
+	nome varchar(150),
+    telefone varchar(20),
+    sexo char(1),
+    endereco varchar(255),
+    cpf char(11) unique,
+    temPlanoSaude bool,
+	CONSTRAINT fk_pessoa_usuario FOREIGN KEY(login) REFERENCES usuario(login)
+);
+
+CREATE TABLE exame(
+	id int primary key auto_increment,
+    custo double not null,
+    descricao varchar(255),
+    tempoJejum time,
+    dataEntrega date,
+    horaEntrega time,
+    isEntregue bool,
+    loginPessoa varchar(20),
+	CONSTRAINT fk_exame_pessoa FOREIGN KEY(loginPessoa) REFERENCES pessoa(login)
+);
+
+INSERT INTO usuario VALUES(
+	'admin', 'admin', true
+);
+
+INSERT INTO pessoa VALUES(
+	'admin', 'Administrador', '', 'M', '', '99999999999', true
+);
--- web/layout/header.jsp
+++ web/layout/header.jsp
@@ -1,5 +1,7 @@
 <header class="">
     <nav class="red darken-4 text-white nav-wrapper">
-        <a href="" class="brand-logo">RedLab</a>
+        <section class="container">
+            <a href="" class="brand-logo">RedLab</a>
+        </section>
     </nav>
 </header>
